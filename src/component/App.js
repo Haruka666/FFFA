0888204a47fcb3e3ee68e81c86071256834137e8
saiid <nadjib.said@efrei.net>
2024-03-27 00:34:45 +0100

    Connexion

--- src/component/App.js
+++ src/component/App.js
@@ -3,9 +3,6 @@ import '../styles/App.css';
 import Accueil from './Accueil';
 import Navbar from './Navbar';
 import Body from './Body';
-import Stats from '../pages/Stats';
-import Joueur from '../pages/Joueur';
-import Divisions from '../pages/Divisions';
 
 function App() {
   return (
--- src/pages/Connexion.js
+++ src/pages/Connexion.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import Navbar from '../component/Navbar';
 import Accueil from '../component/Accueil';
+import React, { useState } from 'react';
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Vérifier les informations d'identification ici, par exemple en utilisant une API ou en les comparant à une base de données
+    // Simulons une connexion réussie ici
+    if (username === 'utilisateur' && password === 'motdepasse') {
+      setLoggedIn(true);
+    } else {
+      alert('Nom d\'utilisateur ou mot de passe incorrect');
+    }
+  };
+
 
 function Connexion() {
   return (
@@ -8,6 +26,30 @@ function Connexion() {
       <Accueil />
       <Navbar></Navbar>
       <div> page Connexion</div>
+      <div>
+      {loggedIn ? (
+        <div>
+          <h1>Bienvenue {username}!</h1>
+          <button onClick={() => setLoggedIn(false)}>Déconnexion</button>
+        </div>
+      ) : (
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Nom d'utilisateur"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Mot de passe"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Se connecter</button>
+        </form>
+      )}
+    </div>
     </div>
   );
 }
